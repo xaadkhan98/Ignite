e04de409fb171824068e66c1ae5e0e49b19d09a4
xaadkhan98 <xaadkhan98@gmail.com>
2024-12-09 15:22:49 +0500

    1. Initial Setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div>Hi</div>
+      <div>Hi</div>
+      <div>Hi</div>
+      <div>Hi</div>
+    </>
+  );
+};
+
+export default App;
